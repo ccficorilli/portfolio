0a1930c3f395643e6dd34e44d5b1f146df571ab0
ccficorilli <ccficorilli@gmail.com>
2018-05-24 10:11:15 -0700

    everything is working fine (navbar, static component, navbtn, image holder) starting to work on the other components

--- src/Static.jsx
+++ src/Static.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+//import './css/static.css';
+import axios from 'axios';
+//import headshot from './img/img.jpg';
 
 class Static extends Component{
     constructor(props){
+        super(props)
+        this.state = {
+            background: []
+        }
+    }
 
-}
-
-
+    componentDidMount(){
+    //this is where the axios call will go for random nature pictures
+    }
 
     render(){
         return(
-            <div className="upper-cell">
+            <div className="static-cell">
                 <div className="img-holder">
-                    <img className="headshot" alt="Something is wrong..." src={headshot} />
+                    <img className="headshot" alt="Something is wrong..." src="./img/img.jpg" />
                 </div>
             </div>
                 
